a54716c86b77406fe11a45c272fa4e06ddb5308e
Seungheon Oh <dan.oh0721@gmail.com>
2020-03-14 20:34:11 -0500

    Task

--- Task.go
+++ Task.go
@@ -11,8 +11,16 @@ import (
 */
 import "C"
 
-type GstTask C.GstTask
+type GstTask struct {
+	GstObject
+}
 type GstTaskState C.GstTaskState
+
+const (
+	GST_TASK_STARTED GstTaskState = 0
+	GST_TASK_STOPPED GstTaskState = 1
+	GST_TASK_PAUSED  GstTaskState = 2
+)
 
 func (t *C.GstTask) impl() *GstTask {
 	return (*GstTask)(unsafe.Pointer(t))
@@ -20,4 +28,59 @@ func (t *C.GstTask) impl() *GstTask {
 
 func (t *GstTask) native() *C.GstTask {
 	return (*C.GstTask)(unsafe.Pointer(t))
+}
+
+// Constructors
+
+//TaskNew: Callback
+
+// Methods
+
+func (t *GstTask) GetPool() *GstTaskPool {
+	return C.gst_task_get_pool(t.native()).impl()
+}
+
+func (t *GstTask) GetState() GstTaskState {
+	return C.GstTaskState(C.gst_task_get_state(t.native()))
+}
+
+func (t *GstTask) Join(tasks ...*GstTask) bool {
+	for _, task := range tasks {
+		if C.gst_task_join(t.native()) == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func (t *GstTask) Pause() bool {
+	return C.gst_task_pause(t.native()) != 0
+}
+
+// SetEnterCallback: callback
+
+// SetLeaveCallback: callback
+
+// SetLock: GRecMutex
+
+func (t *GstTask) SetPool(pool *GstTaskPool) {
+	C.gst_task_set_pool(t.native(), pool.native())
+}
+
+func (t *GstTask) SetState(state GstTaskState) bool {
+	return C.gst_task_set_state(t.native(), C.GstTaskState(state)) != 0
+}
+
+func (t *GstTask) Start() bool {
+	return C.gst_task_start(t.native()) != 0
+}
+
+func (t *GstTask) Stop() bool {
+	return C.gst_task_stop(t.native()) != 0
+}
+
+// Functions
+
+func TaskCleanupAll() {
+	C.gst_task_cleanup_all()
 }
